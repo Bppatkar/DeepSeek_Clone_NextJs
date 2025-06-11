a2cbed2d85a1ef0359a0b77c0ebba463f998457f
Bppatkar <bhanupratappatkar777@gmail.com>
2025-06-11 22:56:29 +0530

    sidebar and clerk functionality is working now come to chat section in sidebar

--- components/PromptBox.jsx
+++ components/PromptBox.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React, { useState } from "react";
 
-const PromptBox = () => {
+const PromptBox = ({ isLoading, setIsLoading }) => {
+  const [prompt, setPrompt] = useState("");
+
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <form
+      className={`w-full  bg-[#404045] p-4 mt-4 rounded-3xl transition-all ${
+        false ? "max-w-3xl" : "max-w-2xl"
+      }`}
+    >
+      <textarea
+        className="outline-none w-full resize-none overflow-hidden break-words bg-transparent"
+        rows={2}
+        placeholder="Message Deepseek "
+        required
+        onChange={(e) => setPrompt(e.target.value)}
+        value={prompt}
+      />
+      <div className="flex items-center justify-between text-sm">
+        <div className="flex items-center gap-2">
+          <p className="flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition">
+            <Image className="h-5" src={assets.deepthink_icon} alt="" />
+            DeepThink (R1)
+          </p>
+          <p className="flex items-center gap-2 text-sm border border-gray-300/40 px-2 py-1 rounded-full cursor-pointer hover:bg-gray-500/20 transition">
+            <Image className="h-5" src={assets.search_icon} alt="" />
+            Search
+          </p>
+        </div>
 
-export default PromptBox
+        <div className="flex items-center gap-2">
+          <Image className="w-4 cursor-pointer" src={assets.pin_icon} alt="" />
+          <button
+            className={`${
+              prompt ? "bg-primary" : "bg-[#71717a]"
+            } rounded-full p-2 cursor-pointer`}
+          >
+            <Image
+              className="w-3.5 aspect-square"
+              src={prompt ? assets.arrow_icon : assets.arrow_icon_dull}
+              alt=""
+            />
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default PromptBox;
