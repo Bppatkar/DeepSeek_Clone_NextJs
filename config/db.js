07e975ec08cdca50552009d30338b65e6299c018
Bppatkar <bhanupratappatkar777@gmail.com>
2025-06-12 11:11:46 +0530

    cocrrecting db file

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ export default async function connectDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(`${process.env.MONGODB_URI}/deepseek`)
+      .connect(process.env.MONGODB_URI)
       .then((mongoose) => {
         mongoose;
       });
