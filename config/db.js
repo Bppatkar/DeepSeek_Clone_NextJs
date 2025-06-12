2634d62dd59fb3faf39066cf330151df989a055f
Bppatkar <bhanupratappatkar777@gmail.com>
2025-06-12 10:39:22 +0530

    changing db name

--- config/db.js
+++ config/db.js
@@ -4,7 +4,11 @@ let cached = global.mongoose || { conn: null, promise: null };
 export default async function connectDB() {
   if (cached.conn) return cached.conn;
   if (!cached.promise) {
-    cached.promise = mongoose.connect(process.env.MONGODB_URI);
+    cached.promise = mongoose
+      .connect(process.env.MONGODB_URI)
+      .then((mongoose) => {
+        mongoose;
+      });
     try {
       cached.conn = await cached.promise;
     } catch (error) {
